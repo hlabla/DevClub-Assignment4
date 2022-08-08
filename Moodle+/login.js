a769ce12b110c2fd97678053c3492e90b41f5fca
hlabla <98545842+hlabla@users.noreply.github.com>
2022-08-08 23:45:58 +0530

    Update login.js

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,21 +1,20 @@
-// console.log("Moodle+ successfully loaded!");
-// const login_element = document.querySelector("div.loginpanel");// Fill the selector for the login element in ""
-// let login_text = login_element.innerText;
-let login_text = 'Log in\nUsername / email\nPassword\n Remember username\nPlease subtract 19 - 88 = \nForgotten your username or password?\nCookies must be enabled in your browser \nSome courses may allow guest access'
+console.log("Moodle+ successfully loaded!");
+const login_element = document.querySelector("div.loginpanel");// Fill the selector for the login element in ""
+let login_text = login_element.innerText;
 let question = login_text.split("\n")[4];
 let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
 const arr = question.split(" ");
 if(arr[1]=="add"){
-  answer = String(Number(arr[2])+Number(arr[4]));
+  answer = Number(arr[2])+Number(arr[4]);
 }
-if(arr[1]=="subtract"){
-  answer = String(Number(arr[2])-Number(arr[4]));
+else if(arr[1]=="subtract"){
+  answer = Number(arr[2])-Number(arr[4]);
 }
-if(arr[2]=="first"){
+else if(arr[2]=="first"){
   answer = arr[4];
 }
 else{
-  answer = arr[5];
+  answer = arr[6];
 }
-// const captcha_input_element = document.querySelector("#valuepkg3"); // Fill the selector for the captcha input element in ""
-// captcha_input_element.value = answer;
+const captcha_input_element = document.querySelector("#valuepkg3"); // Fill the selector for the captcha input element in ""
+captcha_input_element.value = answer;
