cef8d130cc6aa8fa3d9245a2fc38cad7a0e9f2fd
hlabla <98545842+hlabla@users.noreply.github.com>
2022-08-08 23:31:00 +0530

    Update login.js

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,6 +1,7 @@
-console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector("div.loginpanel");// Fill the selector for the login element in ""
-let login_text = login_element.innerText;
+// console.log("Moodle+ successfully loaded!");
+// const login_element = document.querySelector("div.loginpanel");// Fill the selector for the login element in ""
+// let login_text = login_element.innerText;
+let login_text = 'Log in\nUsername / email\nPassword\n Remember username\nPlease subtract 19 - 88 = \nForgotten your username or password?\nCookies must be enabled in your browser \nSome courses may allow guest access'
 let question = login_text.split("\n")[4];
 let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
 const arr = question.split(" ");
@@ -16,5 +17,5 @@ if(arr[2]=="first"){
 else{
   answer = arr[5];
 }
-const captcha_input_element = document.querySelector("#valuepkg3"); // Fill the selector for the captcha input element in ""
-captcha_input_element.value = answer;
+// const captcha_input_element = document.querySelector("#valuepkg3"); // Fill the selector for the captcha input element in ""
+// captcha_input_element.value = answer;
